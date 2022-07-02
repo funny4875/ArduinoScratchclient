1f12af35d0891f17280002ad2afeefc604f34ca7
funny4875 <funny4875@gmail.com>
2022-07-03 07:15:45 +0800

    Add files via upload

---
+++ ArduinoScratchclient.bat
@@ -0,0 +1,8 @@
+set COMport="COM3"
+set username="scratchUser"
+set password="scratPassword"
+set projectID=0
+set uploadVariable=""
+set downloadVariable=""
+ArduinoScratchclient %COMport% %username% %password% %projectID% %uploadVariable% %downloadVariable%
+pause

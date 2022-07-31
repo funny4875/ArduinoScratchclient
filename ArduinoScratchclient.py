6bc75029d6075dd18ef3a7d9308e2e8d937386b3
funny4875 <funny4875@gmail.com>
2022-08-01 00:26:06 +0800

    Update ArduinoScratchclient.py

--- ArduinoScratchclient.py
+++ ArduinoScratchclient.py
@@ -1,22 +1,27 @@
-#dowload scratchclient: 
-#pip install scratchclient
 from scratchclient import ScratchSession
 import time
 import serial
 import sys
+
 COMport = ""
 username = ""
 password = ""
 projectID = 0
 uploadVariable = ""
 downloadVariable = ""
-
-
 if len(sys.argv) < 7:
     print('Need 6 arguments==> argv[1...5]\COMport username password projectID uploadVariable downloadVariable')
     sys.exit()
 else: COMport,username,password,projectID,uploadVariable,downloadVariable = sys.argv[1:]
 
+'''
+COMport = "COM7"
+username = "ccsh_ky"
+password = "123456987"
+projectID = 718312829
+uploadVariable = "power"
+downloadVariable = "motor"
+'''
 try:
     serialPort = serial.Serial(port = COMport, baudrate=9600,
                            bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
@@ -33,10 +38,10 @@ def on_set(variable):
     if variable.name[2:]==downloadVariable:
         try:
             if serialPort.writable():
-                #print(variable.name, variable.value)
+                print(variable.name, variable.value)
                 serialPort.write(bytes([int(variable.value)]))                                
         except:print('write error')
-    
+lastV = 0    
 while(True):
     samples=[]
     for i in range(10):
@@ -45,15 +50,11 @@ while(True):
             T = serialString.decode().strip('\n').strip('\r')
             if len(T)>0:samples.append(int(T))
         else:break
-    
     if len(samples)==0:serialPort.flush();continue
-    V =0
-    if not 0 in samples:
-        V=int(sum(samples)/len(samples))
-    print('write to scratch3 cloud variable',uploadVariable,"=",V)
-    connection.set_cloud_variable(uploadVariable, V)
+    V=int(sum(samples)/len(samples))    
+    if lastV!= V:
+        connection.set_cloud_variable(uploadVariable, V)
+        print('write to scratch3 cloud variable',uploadVariable,"=",V) 
+    lastV = V
     serialPort.flush()
     
-
-
-

6a90960d05920b84f4c3df78b6782787f2d3159f
funny4875 <funny4875@gmail.com>
2022-08-25 00:00:29 +0800

    Update ArduinoScratchclient.py

--- ArduinoScratchclient.py
+++ ArduinoScratchclient.py
@@ -1,41 +1,20 @@
 from scratchclient import ScratchSession
 import time
 import serial
-import sys
-
-COMport = ""
-username = ""
-password = ""
-projectID = 0
-uploadVariable = ""
-downloadVariable = ""
-if len(sys.argv) < 7:
-    print('Need 6 arguments==> argv[1...5]\COMport username password projectID uploadVariable downloadVariable')
-    sys.exit()
-else: COMport,username,password,projectID,uploadVariable,downloadVariable = sys.argv[1:]
-
-'''
-COMport = "COM7"
-username = "ccsh_ky"
-password = "123456987"
+COMport = "COM8"
 projectID = 718312829
-uploadVariable = "power"
-downloadVariable = "motor"
-'''
+username = "ccsh_ky";password = "123456987"
 try:
-    serialPort = serial.Serial(port = COMport, baudrate=9600,
-                           bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
+    serialPort = serial.Serial(port = COMport, baudrate=9600)
 except: print('please check the Arduino connection');sys.exit()
 
 session = ScratchSession(username, password)
 connection = session.create_cloud_connection(projectID)
 
-#connection.set_cloud_variable("x", "123456")
-
 @connection.on("set")
 def on_set(variable):
     global serialPort          
-    if variable.name[2:]==downloadVariable:
+    if variable.name=="☁ D5_W":
         try:
             if serialPort.writable():
                 print(variable.name, variable.value)
@@ -49,12 +28,11 @@ while(True):
             serialString = serialPort.readline()
             T = serialString.decode().strip('\n').strip('\r')
             if len(T)>0:samples.append(int(T))
-        else:break
+        else:break  
     if len(samples)==0:serialPort.flush();continue
     V=int(sum(samples)/len(samples))    
     if lastV!= V:
-        connection.set_cloud_variable(uploadVariable, V)
-        print('write to scratch3 cloud variable',uploadVariable,"=",V) 
+        connection.set_cloud_variable("A0_R", V)
+        print('write to scratch3 cloud variable A0_R=',V) 
     lastV = V
     serialPort.flush()
-    

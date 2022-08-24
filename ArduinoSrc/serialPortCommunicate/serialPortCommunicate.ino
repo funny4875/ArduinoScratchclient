d0363235c348a2ddea1d176cc3ce876518e02da8
funny4875 <funny4875@gmail.com>
2022-08-24 23:55:39 +0800

    Update serialPortCommunicate.ino

--- ArduinoSrc/serialPortCommunicate/serialPortCommunicate.ino
+++ ArduinoSrc/serialPortCommunicate/serialPortCommunicate.ino
@@ -1,17 +1,12 @@
 // C++ code
 //
-void setup()
-{
-  pinMode(A0, INPUT);
-  Serial.begin(9600);
-  pinMode(10, OUTPUT);
-}
-
+#define D5 5
+void setup(){ Serial.begin(9600);}
 void loop()
 {
   Serial.println(analogRead(A0));
   delay(100);
   int a = Serial.read();
-  if(a >= 0) analogWrite(10,a);
-  else digitalWrite(10,LOW);
+  if(a >= 0) analogWrite(D5,a);
+  else analogWrite(D5,0);
 }
